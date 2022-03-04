fc22c0cff818506ac85ffb742bc182af39002b91
NamedRoger <roge.villa.carrera@hotmail.com>
2022-03-03 21:03:04 -0600

    se agrego referencias

--- controller/ventas/listar.php
+++ controller/ventas/listar.php
@@ -1,6 +1,16 @@
 <?php
-include './servicios/servicioVentas.php';
+include '../../clases/DataBase.php';
+include '../../clases/Venta.php';
+include '../../clases/Stock.php';
+include '../../clases/Cliente.php';
+include '../../clases/ProductoVenta.php';
+include '../../clases/Producto.php';
 
+
+include '../servicios/servicioSesion.php';
+include '../servicios/servicioProducto.php';
+include '../servicios/servicioCliente.php';
+include '../servicios/servicioVenta.php';
 
 $ventas = obtenerVentas();
 
